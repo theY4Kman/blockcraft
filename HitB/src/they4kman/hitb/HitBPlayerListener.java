d901037248b23d095cd5dde70280566de8934c4f
theY4Kman <they4kman@gmail.com>
2011-05-06 12:24:48 -0400

    Started Hole in the Blocks

---
+++ HitB/src/they4kman/hitb/HitBPlayerListener.java
@@ -0,0 +1,116 @@
+package they4kman.hitb;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author Zach "theY4Kman" Kanzler
+ */
+public class HitBPlayerListener extends PlayerListener {
+    private final HitB plugin;
+    private HitBServer m_Server = null;
+
+    public HitBPlayerListener(HitB instance) {
+        plugin = instance;
+    }
+    
+    public void onPlayerChat(PlayerChatEvent event)
+    {
+        if (event.isCancelled())
+            return;
+
+        Player player = event.getPlayer();
+        if (!player.isOnline())
+            return;
+        
+        if (m_Server != null)
+        {
+            player.sendMessage("BlockCraft already in progress.");
+            return;
+        }
+        
+        String[] sects = event.getMessage().split(" +", 2);
+        String cmd = sects[0];
+        String[] args = (sects.length > 1 ? sects[1].split(" +") : new String[0]);
+
+        if (!cmd.equals("blockcraft"))
+            return;
+        
+        /* Clear space for our stage. */
+        World world = player.getWorld();
+        Location location = player.getLocation();
+        int direction = Math.round((location.getYaw() / 4)) % 4;
+        
+        /* Directions:
+         * 0: +z
+         * 1: -x
+         * 2: -z
+         * 3: +x
+         */
+        
+        int stopx = location.getBlockX() + 18;
+        int stopy = location.getBlockY() + 9;
+        int stopz = location.getBlockZ() + 18;
+        
+        int startx = location.getBlockX()-18;
+        int startz = location.getBlockZ()-18;
+        int starty = location.getBlockY();
+        
+        switch (direction)
+        {
+        case 0:
+            startz += 18;
+            stopz += 18;
+            break;
+        
+        case 1:
+            startx -= 18;
+            stopx -= 18;
+            break;
+        
+        case 2:
+            startz -= 18;
+            stopz -= 18;
+            break;
+        
+        case 3:
+            startx += 18;
+            stopx += 18;
+        };
+        
+        /* 36x36x7, lwh, x left/right, y up/down, z forward/back */
+        
+        for (int x=startx; x<stopx; x++)
+            for (int z=startz; z<stopz; z++)
+            {
+                for (int y=starty; y<stopy; y++)
+                {
+                    Block b = world.getBlockAt(x, y, z);
+                    b.setType(Material.AIR);
+                }
+                
+                /* Build a roof and floor of glass as well */
+                Block b = world.getBlockAt(x, stopy, z);
+                b.setType(Material.GLASS);
+                b = world.getBlockAt(x, starty-1, z);
+                b.setType(Material.GLASS);
+            }
+        
+        Location origin = new Location(world, startx, starty, startz);
+        m_Server = new HitBServer(plugin, origin, player);
+
+        /* Cancel the command, preventing other plug-ins from seeing it */
+        event.setCancelled(true);
+    }                 
+
+    //Insert Player related code here
+}
+
